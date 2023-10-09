a436616c24d32baf1b6aab2ffb8ccf2146e8b8b3
lovishbansal00 <lovish.bansal.330@gmail.com>
2023-10-09 20:14:44 +0530

    created models for our database

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        requiresd: false,
+    },
+    email: {
+        type: String,
+        requierd: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    likedSongs: {
+        type: Array,
+        required: false,
+    },
+    likedPlaylists: {
+        type: Array,
+        required: false,
+    },
+    subscribedArtists: {
+        type: Array,
+        required: false,
+    },
+});
+
+
+const UserModel = mongoose.model("User", UserSchema);
+
+module.exports = UserModel;
