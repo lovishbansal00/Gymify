ead6651e5a98d12dad76094006b7e200c9bd1bd9
lovishbansal00 <lovish.bansal.330@gmail.com>
2023-10-10 01:21:07 +0530

    added local database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect("mongodb://localhost:27017/spotifyDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get("/", (req, res) => {
     res.send("Hello World");
